ce5ed3b27bd0a25704888dbc25d2c8da3a08c276
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-09-03 18:17:36 +0900

    step2 first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,33 @@
+class BooksController < ApplicationController
+  protect_from_forgery expect: [:destroy]
+  before_action :set_book, only:[:show, :destroy]
+  around_action :action_logger, only:[:destroy]
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html {redirect_to "/"}
+      format.jsopn {head :no_content}
+    end
+  end
+
+  private
+  
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def action_logger
+    logger.info "arround-before"
+    yield
+    logger.info "arround-after"
+  end
+
+end
